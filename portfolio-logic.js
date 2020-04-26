a1e250edcbc8a6d7fb88937d2b96f5c3fe871e8f
Jacob Zirbel <jacobzirbel@gmail.com>
2020-04-26 17:49:11 -0500

    update portfolio

--- portfolio-logic.js
+++ portfolio-logic.js
@@ -1,47 +1,59 @@
 let projects = [
 	{
+		name: "Road Trippers",
+		deployedAt: "https://road-trippers.github.io/road-trip-app/",
+		imgUrl: "assets/images/rtp.png",
+		repoUrl: "https://github.com/Road-Trippers/road-trip-app",
+	},
+	{
 		name: "Harry Potter Hangman",
-		URL: "https://jacobzirbel.github.io/Word-Guess-Game/",
+		deployedAt: "https://jacobzirbel.github.io/Word-Guess-Game/",
 		imgUrl: "assets/images/hangman.png",
+		repoUrl: "https://github.com/jacobzirbel/Word-Guess-Game",
 	},
 	{
 		name: "jQuery Game",
-		URL: "https://jacobzirbel.github.io/unit-4-game/",
+		deployedAt: "https://jacobzirbel.github.io/unit-4-game/",
 		imgUrl: "assets/images/hp.png",
+		repoUrl: "https://github.com/jacobzirbel/unit-4-game",
 	},
 	{
 		name: "Ancient Trivia",
-		URL: "https://jacobzirbel.github.io/TriviaGame/",
-		imgUrl: "assets/images/greek.jpg",
+		deployedAt: "https://jacobzirbel.github.io/TriviaGame/",
+		imgUrl: "assets/images/Greek.jpg",
+		repoUrl: "https://github.com/jacobzirbel/TriviaGame",
 	},
 	{
 		name: "Gif Search",
-		URL: "https://jacobzirbel.github.io/giphy-api/",
-		imgUrl: "assets/images/greek.jpg",
+		deployedAt: "https://jacobzirbel.github.io/giphy-api/",
+		imgUrl: "assets/images/giphy.gif",
+		repoUrl: "https://github.com/jacobzirbel/giphy-api",
 	},
 	{
 		name: "Rock Paper Scissors-multiplayer",
-		URL: "https://jacobzirbel.github.io/RPS-Multiplayer/",
-		imgUrl: "assets/images/greek.jpg",
-	},
-	{
-		name: "Road Trippers",
-		URL: "https://road-trippers.github.io/road-trip-app/",
-		imgUrl: "assets/images/greek.jpg",
+		deployedAt: "https://jacobzirbel.github.io/RPS-Multiplayer/",
+		imgUrl: "assets/images/rps.jpg",
+		repoUrl: "https://github.com/jacobzirbel/RPS-Multiplayer",
 	},
 ];
 
 let jumbo = document.getElementsByClassName("jumbotron")[0];
 let row;
-
+let repos = document.createElement("div");
+repos.style.display = "flex";
+repos.style.flexFlow = "column nowrap";
 projects.forEach((e, i, arr) => {
+	let repoLink = document.createElement("a");
+	repoLink.textContent = e.name;
+	repoLink.setAttribute("href", e.deployedAt);
+	repos.appendChild(repoLink);
 	if (i % 3 === 0) {
 		row = document.createElement("div");
 		row.setAttribute("class", "row justify-content-start");
 	}
 	let newItem = document.createElement("div");
 	newItem.setAttribute("class", "col-md-4 portfolio-img");
-	newItem.onclick = () => window.open(e.URL, "_blank");
+	newItem.onclick = () => window.open(e.deployedAt, "_blank");
 	let newImg = document.createElement("img");
 	newImg.setAttribute("src", e.imgUrl);
 	let newText = document.createElement("p");
@@ -53,3 +65,8 @@ projects.forEach((e, i, arr) => {
 		jumbo.appendChild(row);
 	}
 });
+
+let reposDiv = document.createElement("h4");
+reposDiv.textContent = "Repos:";
+reposDiv.appendChild(repos);
+jumbo.append(reposDiv);
